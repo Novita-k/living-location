0bd3761a0e4e587f59f63aac6c1f7cc534dfe970
Novita-k <myackmyack@gmail.com>
2020-04-18 20:12:58 +0900

    omniAuth google log in 実装
    callback controller作成
    from_omniauthメソッド作成

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should also create an action method in this controller like this:
   # def twitter
   # end
-  def 
+  def google_oauth2
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: @user.provider.capitalize) if is_navigational_format?
+    else
+      session["devise.google_oauth2_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
 
   # More info at:
   # https://github.com/plataformatec/devise#omniauth
