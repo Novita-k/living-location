c99ac7bd6a4ac43db4252ce6c9ab882a13124e61
Novita-k <myackmyack@gmail.com>
2020-05-15 17:04:05 +0900

    mysqlからpostgresqlに変更

--- README.md
+++ README.md
@@ -6,8 +6,10 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
+ruby 2.5.1
 
 * System dependencies
+
 
 * Configuration
 
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2020_05_11_121703) do
 
-  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_121703) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_121703) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "comments", force: :cascade do |t|
     t.text "text", null: false
     t.integer "user_id"
     t.integer "post_id"
@@ -41,7 +44,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_121703) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "images", force: :cascade do |t|
     t.string "image", null: false
     t.integer "post_id"
     t.integer "comment_id"
@@ -49,7 +52,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_121703) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "likes", force: :cascade do |t|
     t.bigint "post_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
@@ -59,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_121703) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.bigint "user_id"
@@ -72,7 +75,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_121703) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
